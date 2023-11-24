9ac995a8a230c27faaf82d79c8b3310cefd5d6d6
zk <kustovweb@gmail.com>
2023-11-24 22:48:35 +0700

    Unit test hw

---
+++ report.md
@@ -0,0 +1,31 @@
+# Отчёт
+
+
+1. Код программы https://github.com/Kustovweb/Unit_test_result/blob/master/sem6_hw.py
+
+
+Код тестов https://github.com/Kustovweb/Unit_test_result/blob/master/sem6_hw.py
+
+
+Отчет pylint https://github.com/Kustovweb/Unit_test_result/blob/master/sem6_test_png/pylint.png
+
+
+Отчет о покрытии тестами https://github.com/Kustovweb/Unit_test_result/blob/master/sem6_test_png/coverage.png
+
+
+2. Объяснение того, какие сценарии покрыты тестами и почему вы выбрали именно эти сценарии.
+
+test_average_value_two
+test_result_two
+test_result_three
+- Проверка с уже известными значениями, позволяет понять что программа работает как задумывалось
+
+
+test_average_value_one
+test_average_value_two
+test_average_type_one
+test_average_type_two
+test_average_type_three
+ - Проверки на неверный тип значения в списках, а также пустой список(списки), либо значение не является списком позволяет обезопасить программу от сбоев или непредвиденной работы.
+ - Проверка исключений, которые прокидывает программа, позволяет понять какой тип исключения генеририрует часть программы.
+
--- sem6_hw.py
+++ sem6_hw.py
@@ -8,13 +8,13 @@ class Compare:
 
     def average_lists(self) -> tuple:
         if not self.nums_list1 or not self.nums_list2:
-            return 0
+            raise Exception('Empty list')
         if not isinstance(self.nums_list1, list) \
                 or not isinstance(self.nums_list2, list):
             raise TypeError('Not list')
         list_all = [*self.nums_list1, *self.nums_list2]
         if not all(map(lambda i: isinstance(i, int), list_all)):
-            raise ValueError("Value Error")
+            raise ValueError('Value Error')
         av_nums_list1 = sum(self.nums_list1) / len(self.nums_list1)
         av_nums_list2 = sum(self.nums_list2) / len(self.nums_list2)
         return av_nums_list1, av_nums_list2
@@ -52,6 +52,11 @@ class TestCompare:
         with pytest.raises(TypeError):
             list_av.compare_lists()
 
+    def test_average_type_three(self):
+        list_av = Compare([], [])
+        with pytest.raises(Exception):
+            list_av.compare_lists()
+
     def test_average_value_one(self):
         list_av = Compare(["1", "1", "1", "1"], [1, 1, 1, 1])
         with pytest.raises(ValueError):
@@ -61,3 +66,5 @@ class TestCompare:
         list_av = Compare([1, 1, 1, 1], ["1", "1", "1", "1"])
         with pytest.raises(ValueError):
             list_av.compare_lists()
+
+
